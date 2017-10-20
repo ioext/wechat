00303e9ab92049b82bfd92ec218598d34dba8c2d
sadkasadasd <2644218983@qq.com>
2017-10-20 19:08:11 +0800

    客户端授权

--- src/OAuth/AccessToken.php
+++ src/OAuth/AccessToken.php
@@ -3,5 +3,17 @@ namespace ioext\wechat\OAuth;
 
 class AccessToken
 {
+    protected $m_sAppId;
+    /**
+     * AccessToken constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    public function GetAppID()
+    {
+        return $this->m_sAppId;
+    }
 
 }
