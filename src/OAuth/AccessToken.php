bed3c3fc1769696d00719965eb90acc8426b7354
sadkasadasd <2644218983@qq.com>
2017-10-23 18:55:31 +0800

    客户端授权secrets

--- src/OAuth/AccessToken.php
+++ src/OAuth/AccessToken.php
@@ -20,6 +20,8 @@ class AccessToken
     public function GetAppSecret()
     {
         return $this->m_sAppSecret;
+        return $this->m_sAppSecret;
+        return $this->m_sAppSecret;
     }
 
 }
