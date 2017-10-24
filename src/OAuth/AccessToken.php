fe0f960b2320896d22e2523880c3b5764510774d
sadkasadasd <2644218983@qq.com>
2017-10-24 17:26:49 +0800

    客户端授权secrets

--- src/OAuth/AccessToken.php
+++ src/OAuth/AccessToken.php
@@ -27,11 +27,6 @@ class AccessToken
         return $this->m_sAppSecret;
         return $this->m_sAppSecret;
         return $this->m_sAppSecret;
-        return $this->m_sAppSecret;
-        return $this->m_sAppSecret;
-        return $this->m_sAppSecret;
-        return $this->m_sAppSecret;
-        return $this->m_sAppSecret;
     }
 
 }
