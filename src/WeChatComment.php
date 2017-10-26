e911b5670f394c7f82535de1dd5097a598e51c57
sadkasadasd <2644218983@qq.com>
2017-10-26 10:21:39 +0800

    客户端授权secrets

--- src/WeChatComment.php
+++ src/WeChatComment.php
@@ -3,20 +3,6 @@ namespace pcappp\wechat;
 
 class WeChatComment
 {
-    public function getAccessToken()
-    {
-
-    }
-
-    public function getAccessCODE()
-    {
-
-    }
-
-    public function getAccessREST()
-    {
-
-    }
 
 
 }
