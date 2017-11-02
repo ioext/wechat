eb07ad81fb0e8adf310b584de46c6aef115a4fc1
sadkasadasd <2644218983@qq.com>
2017-11-02 15:38:44 +0800

    commit ioexxt

--- src/Common/Common.php
+++ src/Common/Common.php
@@ -4,8 +4,11 @@ namespace ioext\wechat\Common;
 
 class Common
 {
-    public static function GetRandomString()
+    /**
+     * 获取随机字符
+     */
+    public static function GetRandomString( $nLength = 10 )
     {
-
+        $sPool = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     }
 }
