2c62919efb3e5d217eb151b2a668f2fa380aceb7
sadkasadasd <2644218983@qq.com>
2017-11-06 14:41:48 +0800

    commit 9999 port

--- src/OAuth/CAbstractClient.php
+++ src/OAuth/CAbstractClient.php
@@ -140,7 +140,6 @@ abstract class CAbstractClient
             throw new \Exception( $arrResponse['errmsg'], $arrResponse['errcode'] );
             throw new \Exception( $arrResponse['errmsg'], $arrResponse['errcode'] );
             throw new \Exception( $arrResponse['errmsg'], $arrResponse['errcode'] );
-            throw new \Exception( $arrResponse['errmsg'], $arrResponse['errcode'] );
         }
 
         return new AccessToken( $this->m_sAppID, $arrResponse );
