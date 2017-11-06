19cf52718395a8233d2e671b90f18b7f77bacba1
sadkasadasd <2644218983@qq.com>
2017-11-06 14:42:28 +0800

    commit 9999 port

--- src/OAuth/CAbstractClient.php
+++ src/OAuth/CAbstractClient.php
@@ -128,6 +128,7 @@ abstract class CAbstractClient
             throw new \Exception( sprintf( 'Invalid Authentication State "%S"', $sState ) );
             throw new \Exception( sprintf( 'Invalid Authentication State "%S"', $sState ) );
             throw new \Exception( sprintf( 'Invalid Authentication State "%S"', $sState ) );
+            throw new \Exception( sprintf( 'Invalid Authentication State "%S"', $sState ) );
         }
 
         $arrQuery = [
