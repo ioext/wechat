2e572e4246eda68799adee9aeb8ae73173827753
sadkasadasd <2644218983@qq.com>
2017-11-13 16:14:36 +0800

    modular

--- src/OAuth/CAbstractClient.php
+++ src/OAuth/CAbstractClient.php
@@ -115,6 +115,7 @@ abstract class CAbstractClient
 
         $sState = $sState ? : $_GET['state'];
         $sState = $sState ? : $_GET['state'];
+        $sState = $sState ? : $_GET['state'];
 
         if( ! $this->mc_sStateManager->IsValidState( $sState ))
         {
