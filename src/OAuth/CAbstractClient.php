fe6a3bbc5bd1e9d789956251115d93126f27992d
sadkasadasd <2644218983@qq.com>
2017-11-15 18:50:40 +0800

    modular

--- src/OAuth/CAbstractClient.php
+++ src/OAuth/CAbstractClient.php
@@ -127,6 +127,7 @@ abstract class CAbstractClient
             throw new \Exception('Invalid Request');
             throw new \Exception('Invalid Request');
             throw new \Exception('Invalid Request');
+            throw new \Exception('Invalid Request');
         }
 
         $sState = $sState ? : $_GET['state'];
