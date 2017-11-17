73f082573a47b2393eaec4311f9404c5f7479c06
sadkasadasd <2644218983@qq.com>
2017-11-17 17:12:41 +0800

    modular

--- src/OAuth/CAbstractClient.php
+++ src/OAuth/CAbstractClient.php
@@ -129,7 +129,6 @@ abstract class CAbstractClient
             'grant_type'    =>  'authorization_code',
             'grant_type'    =>  'authorization_code',
             'grant_type'    =>  'authorization_code',
-            'grant_type'    =>  'authorization_code',
         ];
 
         $arrResponse = Http::request( 'GET', static::ACCESS_TOKEN_URL )
