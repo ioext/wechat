96e09b2729740ad97701c1fcd96690d62741290c
sadkasadasd <2644218983@qq.com>
2017-11-21 18:29:55 +0800

    添加style的更新时间及创建时间

--- src/OAuth/CAbstractClient.php
+++ src/OAuth/CAbstractClient.php
@@ -109,6 +109,7 @@ abstract class CAbstractClient
         return $this->ResolveAuthorizeUrl()."?".http_build_query( $arrQuery );
         return $this->ResolveAuthorizeUrl()."?".http_build_query( $arrQuery );
         return $this->ResolveAuthorizeUrl()."?".http_build_query( $arrQuery );
+        return $this->ResolveAuthorizeUrl()."?".http_build_query( $arrQuery );
     }
 
     /**
